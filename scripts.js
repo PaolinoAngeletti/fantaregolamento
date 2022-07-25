af4461d9d2b0bb27ce3078441de8d686d5d59313
Paolino Angeletti <ang.paolino@alice.it>
2022-07-25 16:57:22 +0200

    scritta tasto nera

--- scripts.js
+++ scripts.js
@@ -10,22 +10,3 @@ function applicaModificatore(daApplicare)
 		x.style.display = "none";
 	}
 }
-
-function generaDocumento()
-{
-	console.log("1");
-	let doc = new Document({ sections: [] });
-	saveDocumentToFile(doc, 'New Document.docx')
-	console.log("2");
-}
-
-function saveDocumentToFile(doc, fileName) {
-  // Create a mime type that will associate the new file with Microsoft Word
-  const mimeType = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-  // Create a Blob object from Packer containing the Document instance and the mimeType
-  Packer.toBlob(doc).then((blob) => {
-    const docblob = blob.slice(0, blob.size, mimeType)
-    // Save the file using saveAs from the file-saver package
-    saveAs(docblob, fileName)
-  })
-}
