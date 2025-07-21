4b33c256d277dcc67f12b8fe251def4d2a37783c
Paolino Angeletti <ang.paolino@alice.it>
2025-07-21 12:54:40 +0200

    substitutions rules refactoring

--- scripts.js
+++ scripts.js
@@ -21,6 +21,7 @@ function loadSectionsScripts() {
 	loadScript("sections/TransferMarketRules.js");
 	loadScript("sections/AccidentReleaseRules.js");
 	loadScript("sections/InsertTeamRules.js");
+	loadScript("sections/SubstitutionsRules.js");
 }
 
 function loadScript(fileName, callback) {
@@ -80,7 +81,7 @@ function creaCodiceHTML() {
 	toReturn = toReturn + TransferMarketRules.produce();
 	toReturn = toReturn + AccidentReleaseRules.produce();
 	toReturn = toReturn + InsertTeamRules.produce();
-	toReturn = toReturn + estraiGestioneSostituzioni();
+	toReturn = toReturn + SubstitutionRules.produce();
 	toReturn = toReturn + estraiDatiCalcoloGiornate();
 	toReturn = toReturn + estraiDatiClassifica();
 	toReturn = toReturn + estraiQuotePremiFinali();
@@ -89,46 +90,6 @@ function creaCodiceHTML() {
 	toReturn = toReturn + "</body>";
 	toReturn = toReturn + "</html>";
 
-	return toReturn;
-}
-
-function estraiGestioneSostituzioni() {
-	var toReturn = Utils.addSectionTitle("Gestione sostituzioni");
-	toReturn = toReturn + estraiNumeroCambi();
-	toReturn = toReturn + estraiGestioneAmmonizioneSenzaVoto();
-	return toReturn;
-}
-
-function estraiNumeroCambi() {
-	var toReturn = "";
-	let cbNessunLimite = Utils.retrieveDomElement("cbNessunLimite");
-	if (cbNessunLimite.checked) {
-		toReturn = Utils.addTextRow("Non vi è nessun limite sul numero di sostituzioni, per cui tutta la panchina può entrare.");
-	} else {
-		let cbTreCambi = Utils.retrieveDomElement("cbTreCambi");
-		if (cbTreCambi.checked) {
-			toReturn = Utils.addTextRow("Sono previste 3 sostituzioni massime, dopodichè si otterrà per le eccedenze un punteggio nullo. L'ordine di entrata sarà dal portiere agli attaccanti.");
-		} else {
-			let cbCinqueCambi = Utils.retrieveDomElement("cbCinqueCambi");
-			if (cbCinqueCambi.checked) {
-				toReturn = Utils.addTextRow("Sono previste 5 sostituzioni massime, dopodichè si otterrà per le eccedenze un punteggio nullo. L'ordine di entrata sarà dal portiere agli attaccanti.");
-			}
-		}
-	}
-	return toReturn;
-}
-
-function estraiGestioneAmmonizioneSenzaVoto() {
-	var toReturn = "";
-	let cbAmmIgnorata = Utils.retrieveDomElement("cbAmmIgnorata");
-	if (cbAmmIgnorata.checked) {
-		toReturn = Utils.addTextRow("Nel caso in cui un giocatore SV (senza voto) venga ammonito, quella ammonizione non avrà alcun impatto né sul giocatore stesso né sul giocatore subentrato; quindi, il subentrato accederà al suo voto in maniera regolare.");
-	} else {
-		let cbAmmScalata = Utils.retrieveDomElement("cbAmmScalata");
-		if (cbAmmScalata.checked) {
-			toReturn = Utils.addTextRow("Nel caso in cui un giocatore SV (senza voto) venga ammonito il malus della ammonizione ricevuta verrà riflesso sul giocatore subentrato, che dunque entrerà con la sua votazione minorata.");
-		}
-	}
 	return toReturn;
 }
 
