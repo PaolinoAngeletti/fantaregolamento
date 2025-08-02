e99f8bd38f7dfdb23aa970a954f877b19e203804
Paolino Angeletti <ang.paolino@alice.it>
2025-08-02 16:44:56 +0200

    Added credits finished management

--- tests/sections/TransferMarketRulesTest.js
+++ tests/sections/TransferMarketRulesTest.js
@@ -14,20 +14,22 @@ function runTransferMarketRulesTests() {
                 realDomDoc.getElementById("etNoteMercato").value = "notes";
                 realDomDoc.getElementById("cbResiduiSi").checked = true;
                 realDomDoc.getElementById("cbResiduiNo").checked = false;
+                realDomDoc.getElementById("taCreditiFiniti").value = "";
 
                 const html = TransferMarketRules.produce(7);
 
                 expect(html).toContain("<h2>7. Gestione mercato");
                 expect(html).toContain("<p>7.1. Per il mercato iniziale sono previsti 100 fantamilioni");
                 expect(html).toContain("<p>7.2. Per le successive sessioni di mercato invece sono previsti 30");
-                expect(html).toContain("<p>7.3. Alla fine di una sessione di mercato, gli eventuali crediti residui");
-                expect(html).toContain("<p>7.4. Sono permessi gli scambi di crediti tra i partecipanti");
-                expect(html).toContain("<p>7.5. Sono permessi i cambi ruolo dei giocatori");
-                expect(html).toContain("<p>7.6. Prevista l'applicazione dello svincolo su acquisto");
-                expect(html).toContain("<p>7.7. Previsto limite di cambi massimi per l'intera competizione");
-                expect(html).toContain("<p>7.8. Previsto limite di cambi massimi per una singola sessione di mercato");
-                expect(html).toContain("<p>7.9. Previsto limite di cambi massimi per ruolo");
-                expect(html).toContain("<p>7.10. notes");
+                expect(html).toContain("<p>7.3. Non è stata specificata nessuna gestione del caso in cui una squadra superi il numero di crediti spendibili");
+                expect(html).toContain("<p>7.4. Alla fine di una sessione di mercato, gli eventuali crediti residui");
+                expect(html).toContain("<p>7.5. Sono permessi gli scambi di crediti tra i partecipanti");
+                expect(html).toContain("<p>7.6. Sono permessi i cambi ruolo dei giocatori");
+                expect(html).toContain("<p>7.7. Prevista l'applicazione dello svincolo su acquisto");
+                expect(html).toContain("<p>7.8. Previsto limite di cambi massimi per l'intera competizione");
+                expect(html).toContain("<p>7.9. Previsto limite di cambi massimi per una singola sessione di mercato");
+                expect(html).toContain("<p>7.10. Previsto limite di cambi massimi per ruolo");
+                expect(html).toContain("<p>7.11. notes");
             });
         });
 
@@ -86,19 +88,33 @@ function runTransferMarketRulesTests() {
             });
         });
 
+        describe("finisched credits notes tests", function () {
+            it("value inserted is empty", function () {
+                realDomDoc.getElementById("taCreditiFiniti").value = "";
+                const html = TransferMarketRules.retrieveFinishedCreditsManagement(14);
+                expect(html).toContain("<p>14.3. Non è stata specificata nessuna gestione del caso in cui una squadra superi il numero di crediti spendibili per il mercato");
+            });
+
+            it("value is not empty", function () {
+                realDomDoc.getElementById("taCreditiFiniti").value = "test-value-1";
+                const html = TransferMarketRules.retrieveFinishedCreditsManagement(6);
+                expect(html).toContain("<p>6.3. Nel caso in cui una squadra superi il numero di crediti spendibili, verrà applicata la seguente strategia: test-value-1</p>");
+            });
+        });
+
         describe("credits propagation on residual test", function () {
             it("propagate residual credits enabled", function () {
                 realDomDoc.getElementById("cbResiduiSi").checked = true;
                 realDomDoc.getElementById("cbResiduiNo").checked = false;
                 const html = TransferMarketRules.retrieveResidualCreditsManagements(2);
-                expect(html).toContain("<p>2.3. Alla fine di una sessione di mercato, gli eventuali crediti residui verranno utilizzati come base di partenza per la successiva sessione di mercato.");
+                expect(html).toContain("<p>2.4. Alla fine di una sessione di mercato, gli eventuali crediti residui verranno utilizzati come base di partenza per la successiva sessione di mercato.");
             });
 
             it("propagate residual credits disabled", function () {
                 realDomDoc.getElementById("cbResiduiSi").checked = false;
                 realDomDoc.getElementById("cbResiduiNo").checked = true;
                 const html = TransferMarketRules.retrieveResidualCreditsManagements(3);
-                expect(html).toContain("<p>3.3. Alla fine di una sessione di mercato, gli eventuali crediti residui verranno ignorati, per cui non verranno utilizzati per le successive sessioni di mercato");
+                expect(html).toContain("<p>3.4. Alla fine di una sessione di mercato, gli eventuali crediti residui verranno ignorati, per cui non verranno utilizzati per le successive sessioni di mercato");
             });
         });
 
@@ -165,7 +181,7 @@ function runTransferMarketRulesTests() {
             it("manage correctly etMassimoScambi = 0", function () {
                 realDomDoc.getElementById("etMaxScambiCompetizione").value = "0";
                 const html = TransferMarketRules.estraiNumeroMassimoCambiCompetizione(6);
-                expect(html).toContain("<p>6.7. Non ci sono limiti relativi al massimo numero di giocatori modificabili per la competizione");
+                expect(html).toContain("<p>6.8. Non ci sono limiti relativi al massimo numero di giocatori modificabili per la competizione");
             });
         });
 
@@ -186,7 +202,7 @@ function runTransferMarketRulesTests() {
                 realDomDoc.getElementById("etMaxScambiCompetizione").value = "0";
                 realDomDoc.getElementById("etMaxScambiSessione").value = "0";
                 const html = TransferMarketRules.estraiNumeroMassimoCambiSessione(3);
-                expect(html).toContain("<p>3.8. Non ci sono limiti relativi al massimo numero di giocatori modificabili in una singola sessione di mercato");
+                expect(html).toContain("<p>3.9. Non ci sono limiti relativi al massimo numero di giocatori modificabili in una singola sessione di mercato");
             });
 
             it("competition value should be greater then session number", function () {
@@ -220,7 +236,7 @@ function runTransferMarketRulesTests() {
                 realDomDoc.getElementById("etMaxScambiSessione").value = "0";
                 realDomDoc.getElementById("etMaxScambiRuolo").value = "0";
                 const html = TransferMarketRules.estraiNumeroMassimoCambiRuolo(3);
-                expect(html).toContain("<p>3.9. Non ci sono limiti relativi al massimo numero di giocatori modificabili per ruolo");
+                expect(html).toContain("<p>3.10. Non ci sono limiti relativi al massimo numero di giocatori modificabili per ruolo");
             });
 
             it("session value should be greater then role number", function () {
@@ -249,7 +265,7 @@ function runTransferMarketRulesTests() {
                 realDomDoc.getElementById("etNoteMercato").value = "hello1 hello2-hello3";
 
                 const html = TransferMarketRules.estraiEventualiNoteAggiuntiveMercato(6);
-                expect(html).toBe("<p>6.10. hello1 hello2-hello3</p>");
+                expect(html).toBe("<p>6.11. hello1 hello2-hello3</p>");
             });
         });
     });
